d4c763db1c1b0e8dc5e6d2a1bea74d21db91458d
Okroshiashvili <n.okroshiashvili@gmail.com>
2019-03-16 12:33:34 +0400

    Add changes

--- publishconf.py
+++ publishconf.py
@@ -21,5 +21,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "dsfabric"
+GOOGLE_ANALYTICS = "UA-136307659-1"
